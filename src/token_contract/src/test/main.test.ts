9807057b5fb4d4dcaf4163d9af9de20c0c2ecada
Weißer Hase <wei3erhase@defi.sucks>
2025-02-06 18:56:20 +0100

    test: running multiple pxes in CI (#13)
    ci: override sandbox docker config to run a secondary PXE
    
    ---------
    
    Co-authored-by: zorzal <zorzal@defi.sucks>

--- src/token_contract/src/test/main.test.ts
+++ src/token_contract/src/test/main.test.ts
@@ -27,15 +27,17 @@ import {
 import { EscrowContract, EscrowContractArtifact } from '@aztec/noir-contracts.js/Escrow';
 import { ContractInstanceDeployerContract } from '@aztec/noir-contracts.js/ContractInstanceDeployer';
 
-const startPXE = async (id: number = 0) => {
-  const { PXE_URL = `http://localhost:${8080 + id}` } = process.env;
-  const pxe = createPXEClient(PXE_URL);
+const createPXE = async (id: number = 0) => {
+  // TODO: we should probably define testing fixtures for this kind of configuration
+  const { BASE_PXE_URL = `http://localhost` } = process.env;
+  const url = `${BASE_PXE_URL}:${8080 + id}`;
+  const pxe = createPXEClient(url);
   await waitForPXE(pxe);
   return pxe;
 };
 
 const setupSandbox = async () => {
-  return startPXE();
+  return createPXE();
 };
 
 async function deployToken(deployer: AccountWallet, minter: AztecAddress) {
@@ -433,7 +435,7 @@ describe('Token', () => {
   }, 300_000);
 });
 
-describe.only('Multi PXE', () => {
+describe('Multi PXE', () => {
   let alicePXE: PXE;
   let bobPXE: PXE;
 
@@ -454,10 +456,10 @@ describe.only('Multi PXE', () => {
     logger = createLogger('aztec:aztec-starter');
     logger.info('Aztec-Starter tests running.');
 
-    alicePXE = await startPXE(0);
-    bobPXE = await startPXE(1);
+    alicePXE = await createPXE(0);
+    bobPXE = await createPXE(1);
 
-    // todo: assert that the used PXEs are actually separate instances?
+    // TODO: assert that the used PXEs are actually separate instances?
 
     aliceWallet = await createAccount(alicePXE);
     bobWallet = await createAccount(bobPXE);
