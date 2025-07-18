87e51ef8e761e53d5ff5329a4a4e5b970bf89ff1
zorzal <zorzal@wonderland.xyz>
2025-07-18 16:56:33 -0300

    ci: uncomment nft benchmark, add logs for token benchmark

--- benchmarks/token_contract.benchmark.ts
+++ benchmarks/token_contract.benchmark.ts
@@ -33,6 +33,8 @@ export default class TokenContractBenchmark extends Benchmark {
 
   async setup(): Promise<TokenBenchmarkContext> {
     const { pxe, store } = await setupPXE();
+    console.log(pxe, store);
+    console.log(await pxe.getNodeInfo());
     const managers = await getInitialTestAccountsManagers(pxe);
     const accounts = await Promise.all(managers.map((acc) => acc.register()));
     const [deployer] = accounts;
