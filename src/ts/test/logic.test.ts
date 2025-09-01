906321c051d675d2dd58c8760214f7ae950e4ce9
zkfrov <frov@wonderland.xyz>
2025-09-01 21:20:44 +0200

    test: withdraw functions tests

--- src/ts/test/logic.test.ts
+++ src/ts/test/logic.test.ts
@@ -18,12 +18,22 @@ import {
 } from '@aztec/stdlib/contract';
 import { getDefaultInitializer } from '@aztec/stdlib/abi';
 import { deriveKeys } from '@aztec/stdlib/keys';
-import { setupPXE } from './utils.js';
+import {
+  setupPXE,
+  deployTokenWithMinter,
+  AMOUNT,
+  expectTokenBalances,
+  wad,
+  deployNFTWithMinter,
+  expectUintNote,
+} from './utils.js';
 import { PXE } from '@aztec/stdlib/interfaces/client';
 import { AztecLmdbStore } from '@aztec/kv-store/lmdb';
 import { getInitialTestAccountsManagers } from '@aztec/accounts/testing';
 import { LogicContractArtifact, LogicContract, EscrowDetailsLogContent } from '../../../artifacts/Logic.js';
 import { EscrowContractArtifact, EscrowContract } from '../../../artifacts/Escrow.js';
+import { TokenContract } from '../../../artifacts/Token.js';
+import { NFTContract } from '../../../artifacts/NFT.js';
 
 /**
  * Deploys the Logic contract.
@@ -105,6 +115,20 @@ export async function deriveContractAddress(
 
 export function grumpkinScalarToFr(scalar: GrumpkinScalar) {
   return new Fr(scalar.toBigInt());
+}
+
+// Check if an address owns a specific NFT in private state
+async function assertOwnsPrivateNFT(
+  nft: NFTContract,
+  tokenId: bigint,
+  owner: AztecAddress,
+  expectToBeTrue: boolean,
+  caller?: AccountWallet,
+) {
+  const n = caller ? nft.withWallet(caller) : nft;
+  const [nfts, _] = await n.methods.get_private_nfts(owner, 0).simulate();
+  const hasNFT = nfts.some((id: bigint) => id === tokenId);
+  expect(hasNFT).toBe(expectToBeTrue);
 }
 
 const setupTestSuite = async () => {
@@ -463,6 +487,160 @@ describe('Logic - Single PXE', () => {
       expect(eventForCarl.keys[1]).toEqual(escrowKeys.masterIncomingViewingSecretKey.toBigInt());
       expect(eventForCarl.keys[2]).toEqual(escrowKeys.masterOutgoingViewingSecretKey.toBigInt());
       expect(eventForCarl.keys[3]).toEqual(escrowKeys.masterTaggingSecretKey.toBigInt());
+    });
+  });
+
+  describe('withdraw', () => {
+    let token: TokenContract;
+
+    beforeEach(async () => {
+      token = (await deployTokenWithMinter(alice, {})) as TokenContract;
+
+      const partialAddressEscrow = await escrow.partialAddress;
+      await pxe.registerAccount(escrowSk, partialAddressEscrow);
+
+      await token
+        .withWallet(alice)
+        .methods.mint_to_private(escrow.instance.address, escrow.instance.address, AMOUNT)
+        .send()
+        .wait();
+    });
+
+    it('logic should be able to withdraw correctly', async () => {
+      const bobPxe = pxe;
+
+      // Add bob to bob's PXE
+      await bobPxe.registerAccount(bob.getSecretKey(), bob.getCompleteAddress().partialAddress);
+
+      // Bob needs to register the escrow contract as an account
+      const partialAddressEscrow = await escrow.partialAddress;
+      await bobPxe.registerAccount(escrowSk, partialAddressEscrow);
+
+      // Bob needs to sync his private state to see the escrow details
+      await token.withWallet(bob).methods.sync_private_state().simulate({});
+
+      await expectTokenBalances(token, escrow.instance.address, wad(0), AMOUNT, bob);
+      await expectTokenBalances(token, bob.getAddress(), wad(0), wad(0), bob);
+
+      const bobWithdrawTx = await logic
+        .withWallet(bob)
+        .methods.test_withdraw(escrow.instance.address, bob.getAddress(), token.instance.address, AMOUNT)
+        .send()
+        .wait();
+
+      await token.withWallet(bob).methods.sync_private_state().simulate({});
+
+      await expectTokenBalances(token, escrow.instance.address, wad(0), wad(0), bob);
+      await expectTokenBalances(token, bob.getAddress(), wad(0), AMOUNT, bob);
+
+      const notes = await bobPxe.getNotes({ txHash: bobWithdrawTx.txHash });
+      expect(notes.length).toBe(1);
+      expectUintNote(notes[0], AMOUNT, bob.getAddress());
+    });
+
+    it('withdrawing less than the balance should succeed', async () => {
+      const bobPxe = pxe;
+
+      const halfAmount = AMOUNT / 2n;
+
+      // Add bob to bob's PXE
+      await bobPxe.registerAccount(bob.getSecretKey(), bob.getCompleteAddress().partialAddress);
+
+      // Bob needs to register the escrow contract as an account
+      const partialAddressEscrow = await escrow.partialAddress;
+      await bobPxe.registerAccount(escrowSk, partialAddressEscrow);
+
+      // Bob needs to sync his private state to see the escrow details
+      await token.withWallet(bob).methods.sync_private_state().simulate({});
+
+      await expectTokenBalances(token, escrow.instance.address, wad(0), AMOUNT, bob);
+      await expectTokenBalances(token, bob.getAddress(), wad(0), wad(0), bob);
+
+      const bobWithdrawTx = await logic
+        .withWallet(bob)
+        .methods.test_withdraw(escrow.instance.address, bob.getAddress(), token.instance.address, halfAmount)
+        .send()
+        .wait();
+
+      await token.withWallet(bob).methods.sync_private_state().simulate({});
+
+      await expectTokenBalances(token, escrow.instance.address, wad(0), halfAmount, bob);
+      await expectTokenBalances(token, bob.getAddress(), wad(0), halfAmount, bob);
+
+      const notes = await bobPxe.getNotes({ txHash: bobWithdrawTx.txHash });
+      expect(notes.length).toBe(2);
+      expectUintNote(notes[0], halfAmount, escrow.instance.address);
+      expectUintNote(notes[1], halfAmount, bob.getAddress());
+    });
+
+    it('withdrawing more than the balance should fail', async () => {
+      await expect(
+        logic
+          .withWallet(bob)
+          .methods.test_withdraw(escrow.instance.address, bob.getAddress(), token.instance.address, AMOUNT + 1n)
+          .send()
+          .wait(),
+      ).rejects.toThrow(/Assertion failed: Balance too low/);
+    });
+  });
+
+  describe('withdraw NFT', () => {
+    let nft: NFTContract;
+    let tokenId: bigint;
+
+    beforeEach(async () => {
+      nft = (await deployNFTWithMinter(alice, {})) as NFTContract;
+      tokenId = 1n;
+
+      const partialAddressEscrow = await escrow.partialAddress;
+      await pxe.registerAccount(escrowSk, partialAddressEscrow);
+
+      await nft.withWallet(alice).methods.mint_to_private(escrow.instance.address, tokenId).send().wait();
+    });
+
+    it('logic should be able to withdraw NFT correctly', async () => {
+      const bobPxe = pxe;
+
+      // Add bob to bob's PXE
+      await bobPxe.registerAccount(bob.getSecretKey(), bob.getCompleteAddress().partialAddress);
+
+      // Bob needs to register the escrow contract as an account
+      const partialAddressEscrow = await escrow.partialAddress;
+      await bobPxe.registerAccount(escrowSk, partialAddressEscrow);
+
+      // Bob needs to sync his private state to see the escrow details
+      await nft.withWallet(bob).methods.sync_private_state().simulate({});
+
+      await assertOwnsPrivateNFT(nft, tokenId, escrow.instance.address, true);
+      await assertOwnsPrivateNFT(nft, tokenId, bob.getAddress(), false);
+
+      const bobWithdrawTx = await logic
+        .withWallet(bob)
+        .methods.test_withdraw_nft(escrow.instance.address, bob.getAddress(), nft.instance.address, tokenId)
+        .send()
+        .wait();
+
+      await nft.withWallet(bob).methods.sync_private_state().simulate({});
+
+      await assertOwnsPrivateNFT(nft, tokenId, escrow.instance.address, false);
+      await assertOwnsPrivateNFT(nft, tokenId, bob.getAddress(), true);
+
+      const notes = await bobPxe.getNotes({ txHash: bobWithdrawTx.txHash });
+      expect(notes.length).toBe(1);
+      expectUintNote(notes[0], tokenId, bob.getAddress());
+    });
+
+    // TODO: Test passes alone, but fails when running with other tests because of this error:
+    // Simulation error: Array must contain at most 100 element(s) (0)
+    // at SchnorrAccount.entrypoint
+    it.skip('withdrawing non-existent NFT should fail', async () => {
+      await expect(
+        logic
+          .withWallet(bob)
+          .methods.test_withdraw_nft(escrow.instance.address, bob.getAddress(), nft.instance.address, tokenId + 1n)
+          .send()
+          .wait(),
+      ).rejects.toThrow(/Assertion failed: nft not found in private to public/);
     });
   });
 });
