734c20301fe65f4b797e2670f4e67f53e223583d
xorsal <zorzal@wonderland.xyz>
2026-03-05 13:57:27 +0000

    chore: add constants file

---
+++ scripts/constants.ts
@@ -0,0 +1,7 @@
+export const DEFAULT_SALT = 1337;
+
+export const NETWORK_URLS: Record<string, string> = {
+  'devnet-2': 'https://v4-devnet-2.aztec-labs.com',
+  testnet: 'https://testnet.aztec-labs.com',
+  sandbox: 'http://localhost:8080',
+};
